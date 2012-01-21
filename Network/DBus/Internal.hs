27f36aa7e0acad029a9c290d7281948d7dcdabb4
Vincent Hanquez <vincent@snarc.org>
2012-01-21 09:14:36 +0000

    add internal

---
+++ Network/DBus/Internal.hs
@@ -0,0 +1,22 @@
+-- |
+-- Module      : Network.DBus.Internal
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : unknown
+--
+
+module Network.DBus.Internal
+	( ObjectPath(..)
+	) where
+
+import Data.String
+import qualified Data.ByteString.UTF8 as UTF8
+import Data.ByteString (ByteString)
+
+newtype ObjectPath = ObjectPath { unObjectPath :: ByteString }
+	deriving (Eq,Ord)
+instance Show ObjectPath where
+	show = show . UTF8.toString . unObjectPath
+instance IsString ObjectPath where
+	fromString = ObjectPath . UTF8.fromString
