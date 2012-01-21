e4bf207cbe664f9aa8a94cf1984a82a759ab00e8
Vincent Hanquez <vincent@snarc.org>
2012-01-21 14:36:05 +0000

    use the notifications service.

--- DBus.hs
+++ DBus.hs
@@ -4,6 +4,7 @@ import System.Posix.User
 import Network.DBus
 import Network.DBus.Actions
 import Network.DBus.Message
+import Network.DBus.MessageType
 import Network.DBus.StdMessage
 import Network.DBus.Type
 import Network.DBus.Signature
@@ -26,6 +27,18 @@ mainDBus uid = do
 		r <- call con dbusDestination msgDBusListNames
 		putStrLn $ show r
 		threadDelay 100000000
+
+	call con "org.freedesktop.Notifications" $ DBusCall "/org/freedesktop/Notifications" "Notify" (Just "org.freedesktop.Notifications")
+		[ DBusString "y"
+		, DBusUInt32 1
+		, DBusString "x"
+		, DBusString "this is a string"
+		, DBusString "this is a o----"
+		, DBusArray (SigString) []
+		, DBusArray (SigDict SigString SigVariant) []
+		, DBusInt32 4000
+		]
+
 
 	forever $ threadDelay 10000
 
