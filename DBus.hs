ef35e122b22bfa45c6205e1f87ff0c3a7d9aa5df
Vincent Hanquez <vincent@snarc.org>
2013-02-19 07:07:31 +0000

    expands tab.

--- DBus.hs
+++ DBus.hs
@@ -14,26 +14,26 @@ import Control.Concurrent
 
 
 main = do
-	con <- establish busGetSession authenticateWithRealUID
-	registerCall con "/" $ calltableFromList
-		[ ("xyz", "example.test.dbus", \s sig b -> putStrLn (show s ++ ": " ++ show b))
-		]
+    con <- establish busGetSession authenticateWithRealUID
+    registerCall con "/" $ calltableFromList
+        [ ("xyz", "example.test.dbus", \s sig b -> putStrLn (show s ++ ": " ++ show b))
+        ]
 
-	forkIO $ forever $ do
-		r <- call con dbusDestination msgDBusListNames
-		putStrLn $ show r
-		threadDelay 100000000
+    forkIO $ forever $ do
+        r <- call con dbusDestination msgDBusListNames
+        putStrLn $ show r
+        threadDelay 100000000
 
-	call con "org.freedesktop.Notifications" $ DBusCall "/org/freedesktop/Notifications" "Notify" (Just "org.freedesktop.Notifications")
-		[ DBusString "y"
-		, DBusUInt32 1
-		, DBusString "x"
-		, DBusString "this is a string"
-		, DBusString "this is a o----"
-		, DBusArray (SigString) []
-		, DBusArray (SigDict SigString SigVariant) []
-		, DBusInt32 4000
-		]
+    call con "org.freedesktop.Notifications" $ DBusCall "/org/freedesktop/Notifications" "Notify" (Just "org.freedesktop.Notifications")
+        [ DBusString "y"
+        , DBusUInt32 1
+        , DBusString "x"
+        , DBusString "this is a string"
+        , DBusString "this is a o----"
+        , DBusArray (SigString) []
+        , DBusArray (SigDict SigString SigVariant) []
+        , DBusInt32 4000
+        ]
 
 
-	forever $ threadDelay 10000
+    forever $ threadDelay 10000
--- Tests.hs
+++ Tests.hs
@@ -15,70 +15,70 @@ import Control.Applicative ((<$>))
 
 -----------------------------------------------------------------------------------
 genSimpleSig =
-	[ return SigByte
-	, return SigBool
-	, return SigInt16
-	, return SigUInt16
-	, return SigInt32
-	, return SigUInt32
-	, return SigInt64
-	, return SigUInt64
-	, return SigDouble
-	, return SigString
-	, return SigObjectPath
-	, return SigSignature
-	, return SigVariant
-	]
+    [ return SigByte
+    , return SigBool
+    , return SigInt16
+    , return SigUInt16
+    , return SigInt32
+    , return SigUInt32
+    , return SigInt64
+    , return SigUInt64
+    , return SigDouble
+    , return SigString
+    , return SigObjectPath
+    , return SigSignature
+    , return SigVariant
+    ]
 
 genSigElem :: Int -> Gen SignatureElem
 genSigElem 0 = oneof genSimpleSig
 genSigElem n = oneof (genSimpleSig ++ [ liftM SigArray subSig, liftM SigStruct (replicateM 2 subSig) ])
-	where
-		subSig :: Gen SignatureElem
-		subSig = genSigElem (n `div` 2)
+    where
+        subSig :: Gen SignatureElem
+        subSig = genSigElem (n `div` 2)
 
 genSig :: Gen Signature
 genSig = replicateM 4 (resize 2 $ sized genSigElem)
 
 -- just there to make it instance of arbitrary without having typesynonym
 newtype DSignature = DSignature Signature
-	deriving (Eq)
+    deriving (Eq)
 
 instance Show DSignature where
-	show (DSignature x) = show x
+    show (DSignature x) = show x
 instance Arbitrary DSignature where
-	arbitrary = DSignature <$> genSig
+    arbitrary = DSignature <$> genSig
 -----------------------------------------------------------------------------------
 
 instance Arbitrary PackedString where
-	arbitrary = fromString <$> arbitrary
+    arbitrary = fromString <$> arbitrary
 
 instance Arbitrary ObjectPath where
-	arbitrary = ObjectPath <$> arbitrary
+    arbitrary = ObjectPath <$> arbitrary
 
 genBody :: Gen (Signature, [DBusValue])
 genBody = genSig >>= \sig -> (mapM sigToBody sig >>= \body ->return (sig,body)) where
-	sigToBody SigByte       = DBusByte <$> arbitrary
-	sigToBody SigBool       = DBusBoolean <$> arbitrary
-	sigToBody SigInt16      = DBusInt16 <$> arbitrary
-	sigToBody SigUInt16     = DBusUInt16 <$> arbitrary
-	sigToBody SigInt32      = DBusInt32 <$> arbitrary
-	sigToBody SigUInt32     = DBusUInt32 <$> arbitrary
-	sigToBody SigInt64      = DBusInt64 <$> arbitrary
-	sigToBody SigUInt64     = DBusUInt64 <$> arbitrary
-	sigToBody SigDouble     = DBusDouble <$> arbitrary
-	sigToBody SigObjectPath = DBusObjectPath <$> arbitrary
-	sigToBody SigString     = DBusString <$> arbitrary
-	sigToBody SigVariant    = sized genSigElem >>= sigToBody >>= return . DBusVariant
-	sigToBody SigSignature  = DBusSignature <$> genSig
-	sigToBody (SigStruct sigs) = mapM sigToBody sigs >>= return . DBusStruct sigs
-	sigToBody (SigArray sig) = mapM sigToBody (replicate 3 sig) >>= return . DBusArray sig
-	sigToBody _              = DBusString <$> arbitrary
+    sigToBody SigByte       = DBusByte <$> arbitrary
+    sigToBody SigBool       = DBusBoolean <$> arbitrary
+    sigToBody SigInt16      = DBusInt16 <$> arbitrary
+    sigToBody SigUInt16     = DBusUInt16 <$> arbitrary
+    sigToBody SigInt32      = DBusInt32 <$> arbitrary
+    sigToBody SigUInt32     = DBusUInt32 <$> arbitrary
+    sigToBody SigInt64      = DBusInt64 <$> arbitrary
+    sigToBody SigUInt64     = DBusUInt64 <$> arbitrary
+    sigToBody SigDouble     = DBusDouble <$> arbitrary
+    sigToBody SigObjectPath = DBusObjectPath <$> arbitrary
+    sigToBody SigString     = DBusString <$> arbitrary
+    sigToBody SigVariant    = sized genSigElem >>= sigToBody >>= return . DBusVariant
+    sigToBody SigSignature  = DBusSignature <$> genSig
+    sigToBody (SigStruct sigs) = mapM sigToBody sigs >>= return . DBusStruct sigs
+    sigToBody (SigArray sig) = mapM sigToBody (replicate 3 sig) >>= return . DBusArray sig
+    sigToBody _              = DBusString <$> arbitrary
 
 data BodyContent = BodyContent Signature [DBusValue]
-	deriving (Show,Eq)
+    deriving (Show,Eq)
 instance Arbitrary BodyContent where
-	arbitrary = genBody >>= \(sig, val) -> return $ BodyContent sig val
+    arbitrary = genBody >>= \(sig, val) -> return $ BodyContent sig val
 -----------------------------------------------------------------------------------
 
 property_signature_marshalling (DSignature x) = (unserializeSignature $ serializeSignature x) == Right x
@@ -86,12 +86,12 @@ property_body_marshalling (BodyContent sig c) = (readBodyRaw LE sig $ writeBody 
 
 assertEq :: (Show a, Eq a) => a -> a -> Bool
 assertEq a b
-	| a == b    = True
-	| otherwise = error ("equality failed:\ngot: " ++ show a ++ "\nexpected " ++ show b)
+    | a == b    = True
+    | otherwise = error ("equality failed:\ngot: " ++ show a ++ "\nexpected " ++ show b)
 
 main = defaultMain tests where
-	tests = [testMarshalling]
-	testMarshalling = testGroup "Marshalling"
-		[ testProperty "Signature" property_signature_marshalling
-		, testProperty "Values" property_body_marshalling
-		]
+    tests = [testMarshalling]
+    testMarshalling = testGroup "Marshalling"
+        [ testProperty "Signature" property_signature_marshalling
+        , testProperty "Values" property_body_marshalling
+        ]
